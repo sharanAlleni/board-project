2886e5799c54f9333bfa5c83076f5a8f741ce945
sharanAlleni <sharanalleni@gmail.com>
2025-10-19 13:19:15 +0530

    deleted unnecessary files

--- app/about/page.tsx
+++
@@ -1,6 +0,0 @@
-const Page= ()=>{
-return ( 
-	<div> ABOUT PAGE </div>
-	);
-};
-export default Page;
--- app/about/sub/page.tsx
+++
@@ -1,6 +0,0 @@
-const Page= ()=>{
-return ( 
-	<div> sub PAGE </div>
-	);
-};
-export default Page;
